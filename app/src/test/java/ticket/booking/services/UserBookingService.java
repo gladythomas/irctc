76c8a600881c9a6145dc4cc8889668afca7e8fcc
gladythomas <gladychaprath1999@gmail.com>
2026-01-29 22:10:56 +0530

    added local db

--- app/src/test/java/ticket/booking/services/UserBookingService.java
+++ app/src/test/java/ticket/booking/services/UserBookingService.java
@@ -1,12 +1,19 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+
 import ticket.booking.entities.User;
 
 public class UserBookingService {
     
     private User user;
+    private static final String USERS_PATH="../localDb/users.json";
+
+    private static  OBJECT_MAPPER= new ObjectMapper();
+
     public UserBookingService(User user){
             this.user=user;
-            
+            File users= new File(USERS_PATH);
     }
 }
